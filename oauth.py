1254ea15954dcafc35fca5692dfbbdc1a0dfade9
bhavybhut <bhavybhut@gmail.com>
2018-04-07 17:31:24 -0400

    insert user google final

--- oauth.py
+++ oauth.py
@@ -1,5 +1,5 @@
 from bottle import request
-import json
+import json, sqlite3
 from google.oauth2 import id_token
 from google.auth.transport import requests
 
@@ -12,7 +12,8 @@ def validateGoogle():
         if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
             raise ValueError('Wrong issuer.')
         userid = idinfo['sub']
-        print(idinfo)
+        #print(idinfo)
+        manageUserInfo(str(idinfo['email']))
         return True
     except ValueError:
         return False
@@ -20,7 +21,21 @@ def validateGoogle():
 def validateFacebook():
     try:
         dict_data = request.json
-        print(dict_data)
         return True
     except ValueError:
         return False
+
+def manageUserInfo(Email):
+    level = 'Level1'
+    conn = sqlite3.connect('./Database/princess.db')
+    c = conn.cursor()
+    c.execute("select EmailAddress from User where EmailAddress=?", (Email,))
+    result = c.fetchall()
+    if(len(result) > 0):
+        c.execute("select UserID, EmailAddress, CurrentGameLevel from User where EmailAddress=?", (Email,))
+        result = c.fetchall()
+    else:
+        c.execute("insert into User (EmailAddress,CurrentGameLevel) values (?,?)", (Email, level))
+        new_id = c.lastrowid
+        conn.commit()
+    c.close()
