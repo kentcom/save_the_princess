b316c5d81fb78faa15960367c079bc615f8e4d7c
SumanBatta <32689829+SumanBatta@users.noreply.github.com>
2018-05-07 19:06:47 -0400

    Add files via upload

--- test1.py
+++ test1.py
@@ -23,30 +23,33 @@ class KentCom_TestCase(unittest.TestCase):
       header1=self.driver.find_element_by_class_name("card-header")
       s='SAVE THE PRINCESS GAME LORE HOME'
       self.assertEqual(s,(header1.text))
+      time.sleep(5)
          
     def test_002__game_rules_page_exists(self):
       elem = self.driver.find_element_by_link_text('Game Rules').click()
       header2=self.driver.find_element_by_tag_name("h2")
       s1='SAVE THE PRINCESS GAME RULES'
       self.assertEqual(s1,(header2.text))	
+      time.sleep(5)
 	 
     def test_03__kent_com_page_exists(self):
       elem = self.driver.find_element_by_link_text('KENTCOM').click()
       header3=self.driver.find_element_by_tag_name("u")
       s2='MISSION:'
       self.assertEqual(s2,(header3.text))
-     
+      time.sleep(5)
     def test_04__kent_com_contact_us_page_exists(self):
       elem = self.driver.find_element_by_link_text('CONTACT US').click()
       header3=self.driver.find_element_by_tag_name("h2")
       s2='SAVE THE PRINCESS GAME CREDITS'
       self.assertEqual(s2,(header3.text))
-    
+      time.sleep(5)
     def test_05__kent_com_game_page_exists(self):
       elem = self.driver.find_element_by_link_text('START GAME').click()
       header3=self.driver.find_element_by_tag_name("h3")
       s2='SAVE THE PRINCESS GAME LOGIN'
       self.assertEqual(s2,(header3.text))
+      time.sleep(5)
    
    
 	  
